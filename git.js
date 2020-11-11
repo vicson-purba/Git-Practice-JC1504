e0be7c2491f44487d389aa74c2b87c838201d8f3
Firliandy <lian.eddy@gmail.com>
2020-11-11 10:07:39 +0700

    added git.js

---
+++ git.js
@@ -0,0 +1,5 @@
+// Push === Upload Code dari local ke Github
+// Pull === Download Code dari Github ke local
+// Origin === var yang menyimpan url github
+// git remote set-url origin urlbaru === mengganti url di dalam origin
+// git remote show origin === melihat isi dari origin (melihat url tujuan push/pull)
